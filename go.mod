25d81ed17c09c173c2bc0dcd9aaba6b9ba10c10f
Nick Huber <nick@nickhuber.ca>
2020-02-16 13:01:48 -0800

    Handle multiple interfaces by default and misc other fixes
    There was a problem with a mq qdisc, and how it makes some fq_codel qdiscs
    that have the same handle of 0: but aren't root qdiscs, causing the program
    to think that it is just looping around on some qdiscs and classes forever
    
    Also use go.mod because apparently thats a good thing to do

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nickhuber/tc-vis
+
+go 1.13
+
+require github.com/vishvananda/netlink v1.1.0
--- tc-vis.go
+++ tc-vis.go
@@ -7,9 +7,12 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
-func handleQdisc(qdiscs []netlink.Qdisc, iface netlink.Link, qdisc netlink.Qdisc, depth int) {
-	classes, _ := netlink.ClassList(iface, qdisc.Attrs().Handle)
+func handleQdisc(qdiscs []netlink.Qdisc, link netlink.Link, qdisc netlink.Qdisc, depth int) {
+	classes, _ := netlink.ClassList(link, qdisc.Attrs().Handle)
 	for _, class := range classes {
+		if class.Attrs().Parent == netlink.HANDLE_ROOT && qdisc.Attrs().Parent != netlink.HANDLE_ROOT {
+			continue
+		}
 		for i := 0; i < depth+1; i++ {
 			fmt.Printf("    ")
 		}
@@ -19,42 +22,55 @@ func handleQdisc(qdiscs []netlink.Qdisc, iface netlink.Link, qdisc netlink.Qdisc
 			class.Attrs().Handle>>16,
 			class.Attrs().Handle&0x0000ffff,
 		)
-		for _, parent_qdisc := range qdiscs {
-			if parent_qdisc.Attrs().Parent == class.Attrs().Handle {
+		for _, cur_qdisc := range qdiscs {
+			if cur_qdisc.Attrs().Parent == class.Attrs().Handle {
 				for i := 0; i < depth+2; i++ {
 					fmt.Printf("    ")
 				}
 				fmt.Printf(
 					"Qdisc %s %x:\n",
-					parent_qdisc.Type(),
-					parent_qdisc.Attrs().Handle>>16,
+					cur_qdisc.Type(),
+					cur_qdisc.Attrs().Handle>>16,
 				)
-				handleQdisc(qdiscs, iface, parent_qdisc, depth+2)
+				handleQdisc(qdiscs, link, cur_qdisc, depth+2)
 			}
 		}
 	}
 }
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("usage: %s interface\n", os.Args[0])
-		os.Exit(1)
-	}
-	iface_name := os.Args[1]
-	iface, _ := netlink.LinkByName(iface_name)
-	if iface == nil {
-		fmt.Printf("%s does not exist\n", iface_name)
-		os.Exit(2)
-	}
-	qdiscs, _ := netlink.QdiscList(iface)
+func handleLink(link netlink.Link, extra_nesting bool) {
+	qdiscs, _ := netlink.QdiscList(link)
 	for idx, qdisc := range qdiscs {
 		switch qdisc.Attrs().Parent {
 		case netlink.HANDLE_ROOT:
+			if extra_nesting {
+				fmt.Printf("    ")
+			}
 			fmt.Printf("Qdisc %s %x: root\n", qdisc.Type(), qdisc.Attrs().Handle>>16)
-			handleQdisc(qdiscs, iface, qdisc, idx)
+			handleQdisc(qdiscs, link, qdisc, idx+1)
 		case netlink.HANDLE_INGRESS:
+			if extra_nesting {
+				fmt.Printf("    ")
+			}
 			fmt.Printf("Qdisc %s %x:\n", qdisc.Type(), qdisc.Attrs().Handle>>16)
-			handleQdisc(qdiscs, iface, qdisc, idx)
+			handleQdisc(qdiscs, link, qdisc, idx+1)
+		}
+	}
+}
+
+func main() {
+	if len(os.Args) > 3 {
+		fmt.Printf("usage: %s [interface]\n", os.Args[0])
+		os.Exit(1)
+	}
+	if len(os.Args) == 2 {
+		link, _ := netlink.LinkByName(os.Args[1])
+		handleLink(link, false)
+	} else {
+		links, _ := netlink.LinkList()
+		for _, link := range links {
+			fmt.Printf("%s\n", link.Attrs().Name)
+			handleLink(link, true)
 		}
 	}
 }
